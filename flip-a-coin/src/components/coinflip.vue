5ebb8b2ef381cc598fd89cb0458127b6b788e288
patriklussi <46919194+patriklussi@users.noreply.github.com>
2021-10-26 14:17:55 +0200

    Started

---
+++ flip-a-coin/src/components/coinflip.vue
@@ -0,0 +1,36 @@
+<template>
+ <div class="wrapper"><button @click="flipIt">Flip a coin</button></div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods: {
+    flipIt(){
+      let coinflip = Math.floor(Math.random() < 0.5);
+      console.log(coinflip);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- flip-a-coin/src/views/Home.vue
+++ flip-a-coin/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <coinflip msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import coinflip from '@/components/coinflip.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    coinflip,
   }
 }
 </script>
